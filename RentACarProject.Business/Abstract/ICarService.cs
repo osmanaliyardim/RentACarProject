f86dfef5d492fb9bdcdcf730cd09c8c52a4ef31c
Osman Ali YARDIM <7006079@bakircay.edu.tr>
2021-09-04 18:13:03 +0300

    AOP olarak ValidationAspect implementasyonu

--- RentACarProject.Business/Abstract/ICarService.cs
+++ RentACarProject.Business/Abstract/ICarService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RentACarProject.Core.Utilities.Results.Abstract;
+using RentACarProject.Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,12 @@ namespace RentACarProject.Business.Abstract
 {
     public interface ICarService
     {
-
+        IDataResult<List<Car>> GetAll();
+        IDataResult<Car> GetById();
+        IDataResult<Car> GetCarsByBrandId(int brandId);
+        IDataResult<Car> GetCarsByColorId(int colorId);
+        IResult Add(Car car);
+        IResult Update(Car car);
+        IResult Delete(Car car);
     }
 }
