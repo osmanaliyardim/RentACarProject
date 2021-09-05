04685ea09c7749ccb5e9875101eb713b019df1b3
Osman Ali YARDIM <7006079@bakircay.edu.tr>
2021-09-05 10:24:09 +0300

    dependency resolvers with autofac

---
+++ RentACarProject.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using RentACarProject.Business.Abstract;
+using RentACarProject.Business.Concrete;
+using RentACarProject.Core.Utilities.Interceptors;
+using RentACarProject.DataAccess.Abstract;
+using RentACarProject.DataAccess.Concrete.EntityFramework;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace RentACarProject.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly)
+                .AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions { Selector = new AspectInterceptorSelector() })
+                .SingleInstance();
+        }
+    }
+}
