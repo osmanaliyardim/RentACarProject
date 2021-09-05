863767c671c4e07258a720f512c24264b5aa6433
Osman Ali YARDIM <7006079@bakircay.edu.tr>
2021-09-05 10:33:08 +0300

    some fixes

---
+++ RentACarProject.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RentACarProject.Core.Utilities.IoC;
+
+namespace RentACarProject.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDependencyResolvers(this IServiceCollection serviceCollection, ICoreModule[] modules)
+        {
+            foreach (var module in modules)
+            {
+                module.Load(serviceCollection);
+            }
+
+            return ServiceTool.Create(serviceCollection);
+        }
+    }
+}
--- RentACarProject.Service.WebAPI/Startup.cs
+++ RentACarProject.Service.WebAPI/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RentACarProject.Core.DependencyResolvers;
+using RentACarProject.Core.Extensions;
+using RentACarProject.Core.Utilities.IoC;
 
 namespace RentACarProject.Service.WebAPI
 {
@@ -25,8 +21,11 @@ namespace RentACarProject.Service.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
+            services.AddDependencyResolvers(new ICoreModule[]{
+                new CoreModule()
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +39,8 @@ namespace RentACarProject.Service.WebAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
