36fd986c977615c1f4b468d4cbac7690348c5ceb
Osman Ali YARDIM <7006079@bakircay.edu.tr>
2021-09-07 09:35:44 +0300

    some fixes

--- RentACarProject.Service.WebAPI/Startup.cs
+++ RentACarProject.Service.WebAPI/Startup.cs
@@ -1,11 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using RentACarProject.Core.DependencyResolvers;
 using RentACarProject.Core.Extensions;
 using RentACarProject.Core.Utilities.IoC;
+using RentACarProject.Core.Utilities.Security.Encryption;
+using RentACarProject.Core.Utilities.Security.JWT;
 
 namespace RentACarProject.Service.WebAPI
 {
@@ -22,6 +26,22 @@ namespace RentACarProject.Service.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
+            var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidAudience = tokenOptions.Audience,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+                };
+            });
 
             services.AddDependencyResolvers(new ICoreModule[]{
                 new CoreModule()
@@ -35,6 +55,12 @@ namespace RentACarProject.Service.WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.ConfigureCustomExceptionMiddleware();
+
+            app.UseCors(builder =>
+                builder.WithOrigins("http://localhost:4200").AllowAnyHeader() //4200 port for Angular
+            );
 
             app.UseHttpsRedirection();
 
